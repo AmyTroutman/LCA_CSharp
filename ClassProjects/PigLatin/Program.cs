41588645470bea96e62bcecb54655950e727c06a
AmyTroutman <amyrtroutman@gmail.com>
2020-01-08 14:09:26 -0600

    my git is rusty. apparently didn't push a commit.

---
+++ ClassProjects/PigLatin/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PigLatin
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Give me a word and I'll translate it into pig latin.");
+        }
+    }
+}
