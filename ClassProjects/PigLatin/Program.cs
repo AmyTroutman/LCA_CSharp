5f63bed86366ac4818cc623c950a1264a248669d
AmyTroutman <amyrtroutman@gmail.com>
2020-01-09 19:49:53 -0600

    pigLatin done (aside from multiconsonant beginnings and handling y). Unloaded ManyMethods because couldn't figure out how to stop it from running.

--- ClassProjects/PigLatin/Program.cs
+++ ClassProjects/PigLatin/Program.cs
@@ -6,18 +6,47 @@ namespace PigLatin
     {
         public static void Main(string[] args)
         {
+            
             Console.WriteLine("Give me a word and I'll translate it into pig latin.");
-            string EnglishWord = Console.ReadLine();
-            //check for vowel at end and beginning. add "yay" if true.
-            if (EnglishWord.StartsWith("a", "e")) && (EnglishWord.EndsWith("a", "e", "i", "o", "u"))
+            string englishWord = Console.ReadLine();
+
+            //char checker
+            char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+
+            //grab first letter
+            string firstLetter = englishWord.Substring(0, 1);
+
+            //grab second letter
+            string lastLetter = englishWord.Substring(englishWord.Length -1);
+
+            //begin building piglatin word
+            //no vowels in word
+            if (englishWord.IndexOfAny(vowels) == -1)
             {
-                Console.WriteLine(EnglishWord + "yay");
+                Console.WriteLine(englishWord + "ay");
             }
-            //check for for vowel at start, consonant at end. add "ay" if true.
-
-            //check for any vowels. if none, add "ay" to the end. IndexOfAny(Char[])
-
-            //check for consonant at start and end, and vowels in between. if true, move consonants before vowel to end of word and add "ay".
+            //first letter is a vowel
+            else if (firstLetter.IndexOfAny(vowels) == 0)
+            {
+                //last letter is a vowel
+                if (lastLetter.IndexOfAny(vowels) == 0)
+                {
+                    Console.WriteLine(englishWord + "yay");
+                }
+                //last letter is a consonant
+                else
+                {
+                    Console.WriteLine(englishWord + "ay");
+                }
+            }
+            //first letter is a consonant
+            else
+            {
+                string pigWord = englishWord.Substring(1, englishWord.Length - 1) + firstLetter;
+                Console.WriteLine(pigWord + "ay");
+                
+            }
+            Console.ReadKey();
         }
     }
 }
