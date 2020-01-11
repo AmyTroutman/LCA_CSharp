2c4f670e9c1b39d8a8930d6567af9f1acdf0096a
AmyTroutman <amyrtroutman@gmail.com>
2020-01-11 15:22:17 -0600

    Did some thinking on how to have PigLatin() repeat.

--- ClassProjects/PigLatin/Program.cs
+++ ClassProjects/PigLatin/Program.cs
@@ -6,57 +6,84 @@ namespace PigLatin
     {
         public static void Main(string[] args)
         {
-            /* can I loop this? So instead of having to rerun, it can keep
-             * accepting new words? Like a function? Or is "public satic
-             * void Main..." the function? */
-            Console.WriteLine("Give me a word and I'll translate it into Pig Latin.");
-            string englishWord = Console.ReadLine();
+            /* There's definitely a way to have it repeat without just queuing
+             * a bunch of "PigLatin()". Would I write another function that asks if you want to translate another word?*/
+            PigLatin();
+            PigLatin();
+            PigLatin();
+            PigLatin();
+            //Repeat();
+            Console.ReadKey();
+        }
 
-            //vowel checker
-            char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+        //Would this be close to how I would repeat the function PigLatin but still be able to exit?
+        /*static void Repeat()
+         * {
+         * Console.WriteLine("Do you want to translate another word? y/n");
+         * char answer = Console.ReadLine();
+         * if (answer == "y")
+         *  {
+         *  run PigLatin();
+         *  }
+         *  else
+         *  {
+         *  Console.ReadKey();
+         *  }
+         * }
+        */
 
-            //grab first letter
-            string firstLetter = englishWord.Substring(0, 1);
+        static void PigLatin()
+        {
+                
+                Console.WriteLine("Give me a word and I'll translate it into Pig Latin.");
+                string englishWord = Console.ReadLine();
 
-            //grab last letter
-            string lastLetter = englishWord.Substring(englishWord.Length -1);
+                //vowel checker
+                char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
 
-            //begin building piglatin word
-            //no vowels in word
-            if (englishWord.IndexOfAny(vowels) == -1)
-            {
-                Console.WriteLine(englishWord + "ay");
-            }
-            //first letter is a vowel
-            else if (firstLetter.IndexOfAny(vowels) == 0)
-            {
-                //last letter is a vowel
-                if (lastLetter.IndexOfAny(vowels) == 0)
-                {
-                    Console.WriteLine(englishWord + "yay");
-                }
-                //last letter is a consonant
-                else
+                //grab first letter
+                string firstLetter = englishWord.Substring(0, 1);
+
+                //grab last letter
+                string lastLetter = englishWord.Substring(englishWord.Length - 1);
+
+                //begin building piglatin word
+                //no vowels in word
+                if (englishWord.IndexOfAny(vowels) == -1)
                 {
                     Console.WriteLine(englishWord + "ay");
                 }
-            }
-            //First letter is a consonant. Accounts for multiple consonants!!
-            else
-            {
-                //Finds the first vowel
-                int firstVowel = englishWord.IndexOfAny(vowels);
-                //Grabs the consonants that precede the first vowel
-                string consonants = englishWord.Substring(0, firstVowel);
-                //Grabs the letters after the last leading consonant
-                string clipWord = englishWord.Substring(firstVowel);
-                //Builds the pigLatin word. 
-                string pigWord = clipWord + consonants + "ay";
-                Console.WriteLine(englishWord + " translated into Pig Latin is " +pigWord);
-                
-            }
-            
-            Console.ReadKey();
+                //first letter is a vowel
+                else if (firstLetter.IndexOfAny(vowels) == 0)
+                {
+                    //last letter is a vowel
+                    if (lastLetter.IndexOfAny(vowels) == 0)
+                    {
+                        Console.WriteLine(englishWord + "yay");
+                    }
+                    //last letter is a consonant
+                    else
+                    {
+                        Console.WriteLine(englishWord + "ay");
+                    }
+                }
+                //First letter is a consonant. Accounts for multiple consonants!!
+                else
+                {
+                    //Finds the first vowel
+                    int firstVowel = englishWord.IndexOfAny(vowels);
+                    //Grabs the consonants that precede the first vowel
+                    string consonants = englishWord.Substring(0, firstVowel);
+                    //Grabs the letters after the last leading consonant
+                    string clipWord = englishWord.Substring(firstVowel);
+                    //Builds the pigLatin word. 
+                    string pigWord = clipWord + consonants + "ay";
+                    Console.WriteLine(englishWord + " translated into Pig Latin is " + pigWord);
+
+                }
+
+              
         }
+        
     }
 }
