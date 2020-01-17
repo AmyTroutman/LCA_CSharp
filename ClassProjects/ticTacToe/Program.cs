27e2c9e2f2c16772d603f0150b2d47f8ffe39850
AmyTroutman <amyrtroutman@gmail.com>
2020-01-16 19:16:52 -0600

    Start of tictactoe.

---
+++ ClassProjects/ticTacToe/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ticTacToe
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //my methods
+            static void placeMarker();
+            static bool isHorizontalWin();
+            static bool isVerticalWin();
+            static bool isDiagonalWin();
+            static bool hasWon();
+            static bool isTie();
+            static void printBoard();
+            //the board
+            String[] row0 = new string[3];
+            String[] row1 = new string[3];
+            String[] row2 = new string[3];
+            String[][] board = new string[3][];
+            board[0] = row0;
+            board[2] = row1;
+            board[2] = row2;
+
+            //current player's turn
+            String currentPlayer = "X";
+
+            //changes current player
+            if (currentPlayer == "X")
+            {
+                currentPlayer = "O";
+                Console.WriteLine(currentPlayer);
+            }
+            else
+            {
+                currentPlayer = "X";
+                Console.WriteLine(currentPlayer);
+            }
+
+
+        }
+     
+    }
+}
