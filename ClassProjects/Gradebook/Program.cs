1e0375fac11fd54e82913196c73cf6f68a13cd13
AmyTroutman <amyrtroutman@gmail.com>
2020-01-25 17:50:59 -0600

    Prints correctly now. put in a try/catch for the convert bit. But, it still adds the student without a value. So if you try to input the correct value, it tells you the student exists. Can I put an if/else loop inside a try/catch?

--- ClassProjects/Gradebook/Program.cs
+++ ClassProjects/Gradebook/Program.cs
@@ -18,9 +18,9 @@ namespace Gradebook
             do
             {
                 Console.WriteLine("Enter Student's name. If you are done entering all students, type quit.");
-                student = Console.ReadLine().ToLower();
+                student = Console.ReadLine().ToUpper();
 
-                if (student == "quit")
+                if (student == "QUIT")
                 {
                     hasQuit = true;
                 }
@@ -29,12 +29,12 @@ namespace Gradebook
                     Console.WriteLine("Enter the student's grades, separated by a space (example: 100 90 97 85).");
                     grades = Console.ReadLine();
                     ProcessGrades(grades);
-                    //try catch here to check for duplicate student
+                   
                     try
                     {
                         studentRecord.Add(student, newGrades);
                     }
-                    catch
+                    catch (ArgumentException)
                     {
                         Console.WriteLine("Student already exists.");
                     }
@@ -47,64 +47,65 @@ namespace Gradebook
 
         public static void ProcessGrades(string grades)
         {
-
-            int highest = 0;
-            int lowest = 100;
-            int average = 0;
-            int sum = 0;
-            //int average = 0;
-
-            //first, split the values into an array, split on " "
-            string[] gradeArray = grades.Split(' ');
-
-            //convert to int
-
-            int[] numGradeArray = Array.ConvertAll(gradeArray, s => int.Parse(s));
-            //find the highest grade, save to int highest
-
-            foreach (var i in numGradeArray)
+            try
             {
-                if (i > highest)
+                int highest = 0;
+                int lowest = 100;
+                int average = 0;
+                int sum = 0;
+                //int average = 0;
+
+                //first, split the values into an array, split on " "
+                string[] gradeArray = grades.Split(' ');
+
+                //convert to int
+
+                int[] numGradeArray = Array.ConvertAll(gradeArray, s => int.Parse(s));
+                //find the highest grade, save to int highest
+
+                foreach (var i in numGradeArray)
                 {
-                    highest = i;
+                    if (i > highest)
+                    {
+                        highest = i;
+                    }
                 }
-            }
 
-            //find the lowest grade, save to int lowest
+                //find the lowest grade, save to int lowest
 
-            foreach (var i in numGradeArray)
-            {
-                if (i < lowest)
+                foreach (var i in numGradeArray)
                 {
-                    lowest = i;
+                    if (i < lowest)
+                    {
+                        lowest = i;
+                    }
                 }
-            }
 
-            //find the average, save to int average
-            foreach (var i in numGradeArray)
+                //find the average, save to int average
+                foreach (var i in numGradeArray)
+                {
+                    sum += i;
+                }
+                average = sum / numGradeArray.Length;
+
+
+                //Console.WriteLine(processedGrades);
+                newGrades = "Highest grade: " + highest + ". Lowest grade: " + lowest + ". Average grade: " + average + ".";
+            }
+            catch
             {
-                sum += i;
+                Console.WriteLine("I think one of those wasn't a number!");
             }
-            average = sum / numGradeArray.Length;
-
-
-            //Console.WriteLine(processedGrades);
-            newGrades = "Highest grade: " + highest + ". Lowest grade: " + lowest + ". Average grade: " + average + ".";
-
         }
 
         public static void PrintGradebook(Dictionary<string, string> studentRecord)
         {
             //print it
-
-            foreach (var key in studentRecord.Keys)
+            Console.WriteLine();
+            foreach (KeyValuePair<string, string> kvp in studentRecord)
             {
-                Console.WriteLine("Student: " + key);
-                foreach (var value in studentRecord[key])
-                {
-                    Console.WriteLine(value);
-                }
-
+                Console.WriteLine("{0} {1}",
+                    kvp.Key, kvp.Value);
             }
 
         }
