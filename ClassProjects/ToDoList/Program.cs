d40a21f0004b6068bfaa601fd139164756424fd0
AmyTroutman <amyrtroutman@gmail.com>
2020-01-30 11:22:32 -0600

    Got the prompts, beginning of MakeList class, and toDoList dictionary.

---
+++ ClassProjects/ToDoList/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ToDoList
+{
+    class MainClass
+    {
+         
+        public static void Main(string[] args)
+        {
+            bool imDone = false;
+            Console.WriteLine("Let's make a list!");
+            do
+            {
+                Console.WriteLine("What do you need to do?");
+                string task = Console.ReadLine();
+                Console.WriteLine("When do you need to get this done by? (mm/dd/yy)");
+                string date = Console.ReadLine();
+                Console.WriteLine("And how important is this? (high, medium, low)");
+                string priority = Console.ReadLine();
+                Console.WriteLine("Do you need to do anything else?");
+                string noMore = Console.ReadLine().ToLower();
+                if (noMore == "no")
+                {
+                    imDone = true;
+                }
+                else
+                {
+                    //method from MakeList class to add task, date, and priority
+                    //as value to dictionary key task1
+                }
+            } while (imDone == false);
+            //
+        }
+        
+    }
+    class MakeList
+    {
+        public Dictionary<string, string[]> toDoList = new Dictionary<string, string[]>();
+    }
+}
