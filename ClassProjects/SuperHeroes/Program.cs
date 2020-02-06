4d5cd406124c8a1969159c3e9e31d1b003fddd2c
AmyTroutman <amyrtroutman@gmail.com>
2020-02-05 21:30:40 -0600

    I think all Classes are good, just need to set up print in Main.

--- ClassProjects/SuperHeroes/Program.cs
+++ ClassProjects/SuperHeroes/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SuperHeroes
 {
@@ -6,7 +7,15 @@ namespace SuperHeroes
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Person p1 = new Person("Robert", "Bob");
+            Hero h1 = new Hero("Fireman", null, "John", "Fireball");
+            Villain v1 = new Villain("Fireman", "AquaMan", null);
+            List<Person> mereMortals = new List<Person>();
+            List<Hero> goodGuys = new List<Hero>();
+            List<Villain> badDudes = new List<Villain>();
+            mereMortals.Add(p1);
+            goodGuys.Add(h1);
+            badDudes.Add(v1);
         }
     }
     public class Person
@@ -17,6 +26,12 @@ namespace SuperHeroes
         {
             this.Name = name;
             this.NickName = nickName;
+        }
+        //Why do I do this? Somewhere I'm supposed to ToString...
+        public override String ToString() { return String.Format("{0}", Name); }
+        public void PrintGreeting()
+        {
+            Console.WriteLine("Hello, my name is {0} but you can call me {1}.", Name, NickName);
         }
     }
     public class Hero : Person
@@ -30,7 +45,11 @@ namespace SuperHeroes
             this.SuperPower = superPower;
             NickName = null;
         }
-        
+        public new void PrintGreeting()
+        {
+            Console.WriteLine("I am {0}. When I am {1}, my super power is { 2 }!", RealName, Name, SuperPower);
+        }
+
     }
     public class Villain :  Person
     {
@@ -40,6 +59,11 @@ namespace SuperHeroes
             this.Name = name;
             this.NickName = null;
             this.Nemesis = nemesis;
+        }
+        public new void PrintGreeting()
+        {
+            Console.WriteLine("I am {0}. Have you seen {1}?!!",
+            Nemesis, Name);
         }
     }
 }
