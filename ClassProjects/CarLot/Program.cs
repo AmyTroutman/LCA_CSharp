0072136ec26aabf6b8741e4ca5b8984beac3abb2
AmyTroutman <amyrtroutman@gmail.com>
2020-02-06 20:14:27 -0600

    Beginning of CarLot. Barebones classes.

---
+++ ClassProjects/CarLot/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CarLot
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class CarLot
+    {
+
+    }
+
+    abstract class Vehicle
+    {
+
+    }
+
+    class Car : Vehicle
+    {
+
+    }
+
+    class Truck : Vehicle
+    {
+
+    }
+
+}
