bc2a3dc89aacb21d24927573e4d6fc692220ad1b
AmyTroutman <amyrtroutman@gmail.com>
2020-02-09 22:35:12 -0600

    Now prints the inventory, and I finally remembered to use the fancy method.

--- ClassProjects/CarLot/Program.cs
+++ ClassProjects/CarLot/Program.cs
@@ -1,16 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 
-//my enums, they should be for like make, model, type, hauling?
-//an enum for each field.
-//public enum Make { Mazda, Toyota, Ford, Honda }
-//public enum Model { Mazda3, Mazda5, Corolla, Tacoma, F150, Taurus, Civic, Fit }
-//public enum Type { Sedan, Coupe, Hatchback }
-//public enum HaulingCap { Ton, MegaTon, Tonne, GrossTon }
-
 namespace CarLot
 {
-    class MainClass
+    class Program
     {
         public static void Main(string[] args)
         {
@@ -19,26 +12,34 @@ namespace CarLot
             CarLot cl2 = new CarLot("Bob's O-K Car-ral", new List<Vehicle>());
 
             //make some cars
-            Car car01 = new Car("ABC1234", "Mazda", "Mazda3", 22000, "hatchback", 5);
+            Car car01 = new Car("ART1985", "Mazda", "Mazda3", 22000, "hatchback", 5);
             Car car02 = new Car("DEF5678", "Honda", "Fit", 32000, "hatchback", 5);
-            Truck truck01 = new Truck("GHI9012", "Toyota", "Tacoma", 54000, "XXl", "20 gross tons");
+            Car car03 = new Car("JFK1234", "Nissan", "Sentra", 12000, "sedan", 4);
+            Car car04 = new Car("RMT1086", "Toyota", "Corolla", 32000, "sedan", 4);
+            Truck truck01 = new Truck("GHI9012", "Toyota", "Tacoma", 54000, "XXL", "20 gross tons");
             Truck truck02 = new Truck("TRU3452", "Ford", "F150", 65000, "X-Tendo-Matic", "5 tonnes");
+            Truck truck03 = new Truck("EFT2932", "Mazda", "Mazda", 4000, "Regular", "1 Metric Ton");
+           
 
-            //add to list            
+            //add to lists            
             cl1.ToList(car01);
             cl1.ToList(truck02);
+            cl1.ToList(car03);
             cl2.ToList(truck01);
             cl2.ToList(car02);
+            cl2.ToList(car04);
+            cl2.ToList(truck03);
 
-            //print inventory
-            PrintInv();
+            //print inventories
+            cl1.PrintInv();
+            cl2.PrintInv();
         }
     }
 
     class CarLot
     {
         string LotName { get; set; }
-        List<Vehicle> Vehicles = new List<Vehicle>();
+        public List<Vehicle> Vehicles = new List<Vehicle>();
 
         public CarLot (string lotName, List<Vehicle> vehicles)
         {
@@ -46,17 +47,27 @@ namespace CarLot
             this.Vehicles = vehicles;
         }
 
-        //the list add thingy
+        //method to add cars (objects) to lots (lists)
         public void ToList(Vehicle vehicle)
         {
                 Vehicles.Add(vehicle);
         }
+
+        //method for printing inventory
         public void PrintInv()
         {
-            //mebbe like this??
-            foreach (object car in Vehicles)
+            //get number of cars in inv
+            int InvCount = Vehicles.Count;
+
+            //print lot info
+            //remembered to do the ${ } thing!
+            Console.WriteLine($"{LotName} - Inventory:\n");
+            Console.WriteLine($"Number of vehicles in inventory:\n {InvCount}");
+
+            //print info on each car in lot
+            foreach (var item in Vehicles)
             {
-                Console.WriteLine(car);
+                Console.WriteLine(item.Info());
             }
         }
     }
@@ -74,8 +85,12 @@ namespace CarLot
             this.Plate = plate;
             this.Make = make;
             this.Model = model;
-            this.Price = price;
-            
+            this.Price = price;            
+        }
+        //japes
+        public virtual string Info()
+        {
+            return "If you can see this, something went wrong...";
         }
     }
 
@@ -93,6 +108,17 @@ namespace CarLot
             this.Type = type;
             this.Doors = doors;
         }
+
+        public override string Info()
+        {
+            return  
+                $"\nLicense Plate Number: {Plate}" +
+                $"\nMake: {Make}" +
+                $"\nModel: {Model}" +
+                $"\nPrice: ${Price}" +
+                $"\nType: {Type}" +
+                $"\nNumber Of Doors: {Doors}\n";
+        }
     }
 
     class Truck : Vehicle
@@ -109,6 +135,17 @@ namespace CarLot
             this.BedSize = bedSize;
             this.HaulingCap = haulingCap;
             
+        }
+
+        public override string Info()
+        {
+            return 
+                $"\nLicense Plate Number: {Plate}" +
+                $"\nMake: {Make}" +
+                $"\nModel: {Model}" +
+                $"\nPrice: ${Price}" +
+                $"\nType: {BedSize}" +
+                $"\nHauling Capacity: {HaulingCap}\n";
         }
     }
 
