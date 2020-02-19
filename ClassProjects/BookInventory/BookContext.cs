d777f93ab50c76e7c2382db43c60b42f7f1e519e
AmyTroutman <amyrtroutman@gmail.com>
2020-02-19 10:15:08 -0600

    Initial BookInventory commit

---
+++ ClassProjects/BookInventory/BookContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using System.IO;
+
+namespace BookInventory
+{
+    class BookContext : DbContext
+    {
+        public DbSet<Book> MyLibrary { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // get the directory the code is being executed from
+            DirectoryInfo ExecutionDirectory = new DirectoryInfo(AppContext.BaseDirectory);
+
+            // get the base directory for the project
+            DirectoryInfo ProjectBase = ExecutionDirectory.Parent.Parent.Parent;
+
+            // add 'library.db' to the project directory
+            String DatabaseFile = Path.Combine(ProjectBase.FullName, "library.db");
+
+            // to check what the path of the file is, uncomment the file below
+            //Console.WriteLine("using database file :"+DatabaseFile);
+
+            optionsBuilder.UseSqlite("Data Source=" + DatabaseFile);
+        }
+    }
+}
---
+++ ClassProjects/BookInventory/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace BookInventory
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            bool stop = false;
+            BookContext context = new BookContext();
+            context.Database.EnsureCreated();
+            do
+            {
+                Console.WriteLine("What is the name of the book you'd like to add to the library?");
+                string title = Console.ReadLine();
+                Console.WriteLine("Who is the author of {0}?", title);
+                string author = Console.ReadLine();
+                Book newBook = new Book(title, author);
+                context.MyLibrary.Add(newBook);
+                context.SaveChanges();
+                Console.WriteLine("{0} was added to your library! Would you like to add another book?");
+                string answer = Console.ReadLine().ToLower();
+                if (answer == "no")
+                {
+                    stop = true;
+                }
+            } while (stop == false);
+            Console.WriteLine("These are the books in your library:");
+
+            foreach (Book b in context.MyLibrary)
+            {
+                Console.WriteLine("{0}: {1} {2}", b.Id, b.Title, b.Author);
+            }
+        }
+    }
+}
